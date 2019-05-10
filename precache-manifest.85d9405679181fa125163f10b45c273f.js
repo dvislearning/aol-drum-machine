836cdfa19f26ccd2f95ab9ed21da359fa88bb933
DV <DVislearning@gmail.com>
2019-05-10 15:47:24 -0700

    Updates

---
+++ precache-manifest.85d9405679181fa125163f10b45c273f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0760650ebd6faddb4d0f",
+    "url": "/aol-drum-machine/static/css/main.1420d9c0.chunk.css"
+  },
+  {
+    "revision": "0760650ebd6faddb4d0f",
+    "url": "/aol-drum-machine/static/js/main.24592c2b.chunk.js"
+  },
+  {
+    "revision": "1b571d3a59f9564958b5",
+    "url": "/aol-drum-machine/static/js/runtime~main.d6126d39.js"
+  },
+  {
+    "revision": "8ab49e41bce24466c49e",
+    "url": "/aol-drum-machine/static/js/2.df565963.chunk.js"
+  },
+  {
+    "revision": "b21dbca7c778aec6eefffd498daf5cee",
+    "url": "/aol-drum-machine/static/media/Buddy-In.b21dbca7.mp3"
+  },
+  {
+    "revision": "06f2cbe48a59d810cc28e94df45f4918",
+    "url": "/aol-drum-machine/static/media/Buddy-Out.06f2cbe4.mp3"
+  },
+  {
+    "revision": "6d9462e66bc7e1e4d0aa5e07eb35d155",
+    "url": "/aol-drum-machine/static/media/Drop.6d9462e6.mp3"
+  },
+  {
+    "revision": "4f040a90d466927eb828f1e67e818270",
+    "url": "/aol-drum-machine/static/media/File-Done.4f040a90.mp3"
+  },
+  {
+    "revision": "f4200c5fa79c3f5ce7c1bd65d016570c",
+    "url": "/aol-drum-machine/static/media/Goodbye.f4200c5f.mp3"
+  },
+  {
+    "revision": "f96c099607eb0340d02289473b29636e",
+    "url": "/aol-drum-machine/static/media/im.f96c0996.mp3"
+  },
+  {
+    "revision": "63e8a01a1d983362fb162cefce36086f",
+    "url": "/aol-drum-machine/static/media/Got-Pics.63e8a01a.mp3"
+  },
+  {
+    "revision": "f8a11654e4e9545e5d600533e07f9720",
+    "url": "/aol-drum-machine/static/media/Welcome.f8a11654.mp3"
+  },
+  {
+    "revision": "5dc90c919139e3473ad0cf9c29ddb851",
+    "url": "/aol-drum-machine/static/media/You-Got-Mail.5dc90c91.mp3"
+  },
+  {
+    "revision": "0b0a5c32c2bb089558d3e078d0302856",
+    "url": "/aol-drum-machine/static/media/aolpic.0b0a5c32.svg"
+  },
+  {
+    "revision": "ebc02ecac45e0fca94fc236c4de458b5",
+    "url": "/aol-drum-machine/index.html"
+  }
+];
